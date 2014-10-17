6f25448e7632c47318c69c6568859e251d2ca1d8
zhu91aizhu <zhu91aizhu@163.com>
2014-10-17 15:13:12 +0800

    修改程序入口，添加项目配置文件参数

--- RequestServer/main.py
+++ RequestServer/main.py
@@ -1,7 +1,12 @@
 # _*_ coding:utf-8 _*_
 """程序入口模块"""
+import sys
 from requestcommand import RequestCommand
 
 if __name__ == "__main__":
-    REQ_CMD = RequestCommand(r"requesturl.xml")
+    if len(sys.argv) == 1:
+        print "no projects xml file."
+        exit(1)
+    PRO_CONF = sys.argv[1]
+    REQ_CMD = RequestCommand(PRO_CONF)
     REQ_CMD.cmdloop()
